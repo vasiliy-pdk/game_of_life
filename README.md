e91d04b4cd237a8713be08ef3684e5f5c366faf4
Vasyl Pedak <truvazia@gmail.com>
2019-12-03 19:55:43 +0000

    Add .editorconfig

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-My simplistic implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) as a part of mine 
-deliberate practice of TDD and Simple Design in Ruby. Inspired by the [Global Day Of Code Retreat in 2019 with 8th Light](http://geekvasyl.me/2019/11/17/The-Global-Day-of-Coderetreat-with-8th-Light/). 
+My simplistic implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) as a part of mine
+deliberate practice of TDD and Simple Design in Ruby. Inspired by the [Global Day Of Code Retreat in 2019 with 8th Light](http://geekvasyl.me/2019/11/17/The-Global-Day-of-Coderetreat-with-8th-Light/).
 
 # How to run?
 
--- lib/console_renderer.rb
+++ lib/console_renderer.rb
@@ -1,4 +1,4 @@
-# Renderer accepts the grid and formats to string then clears 
+# Renderer accepts the grid and formats to string then clears
 # the console and outputs
 #
 # @TODO: Improve it visually:
@@ -6,11 +6,12 @@
 # 2. Provide some information at the first line
 # 3. Draw the field better:
 # 3.1 Colours
-# 3.2 Unicode symbols: https://charbase.com/ output by "\u2620"; ASCII symbols: 
-# 3.3 Box drawing: https://charbase.com/block/box-drawing ; https://en.wikipedia.org/wiki/Block_Elements 
+# 3.2 Unicode symbols: https://charbase.com/ output by "\u2620"; ASCII symbols:
+# 3.3 Box drawing: https://charbase.com/block/box-drawing ; https://en.wikipedia.org/wiki/Block_Elements
 #
 # Notes: more on ASCII https://en.wikipedia.org/wiki/ASCII ; https://en.wikipedia.org/wiki/ANSI_art ; https://en.wikipedia.org/wiki/ANSI_escape_code
-# http://www.termsys.demon.co.uk/vtansi.htm  
+# http://www.termsys.demon.co.uk/vtansi.htm
+
 class ConsoleRenderer
   HOME_CURSOR = "\e[H"
   CLEAR_SCREEN = "\e[2J"
@@ -29,7 +30,7 @@ class ConsoleRenderer
 
   def format(grid)
     grid.map do |row|
-      row.map { |cell| format_cell(cell) }.join 
+      row.map { |cell| format_cell(cell) }.join
     end.join("\n") + "\n"
   end
 
