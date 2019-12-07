ce99e944ba3c4968fd4835bb9cbe593f7f2ceb4f
Vasyl Pedak <truvazia@gmail.com>
2019-12-07 12:55:21 +0000

    Add notes

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,16 @@ class Game
 
   def run
     begin
+      # @TODO: In order to enable more control over the game, we need to collect commands here, and find a way to
+      # pass the input to the World.
+
       renderer.render world.simulate
+
+      # @TODO: this sleep does not belong here. The Game object should be abstract enough to be useful in any game.
+      # Currently it assumes the speed, but potentially it might be not suitable when
+      # A) we want to run GoL or another game faster
+      # B) world simulation could take longer therefore decreasing perceived speed.
+      # Here are more details on game loops: https://gameprogrammingpatterns.com/game-loop.html
       sleep 0.1
     end while should_proceed?
   end
